91ce75b13cbb1b4f22b4f0b6ee77454659dce391
Jessica Yang <jessicayang71@gmail.com>
2018-05-07 14:36:06 -0700

    losing my mind

--- app.py
+++ app.py
@@ -11,12 +11,11 @@ PAT = ''
 @app.route('/', methods=['GET'])
 def handle_verification():
   print("Handling Verification.")
-  if request.args.get("hub.verify_token") == 'my_voice_is_my_password_verify_me':
+  if request.args.get('hub.verify_token') == 'my_voice_is_my_password_verify_me':
     print("Verification successful!")
     return request.args.get('hub.challenge')
   else:
     print("Verification failed!")
-    print('hub.verify_token')
     return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
