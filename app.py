a621f19165c1ca599dc35f55793657c6ed4c8ead
Jessica Yang <jessicayang71@gmail.com>
2018-05-07 11:55:41 -0700

    fix print statements again

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ PAT = ''
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-  print "Handling Verification."
+  print("Handling Verification.")
   if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
-    print "Verification successful!"
+    print("Verification successful!")
     return request.args.get('hub.challenge', '')
   else:
-    print "Verification failed!"
+    print("Verification failed!")
     return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
