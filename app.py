93a295e18c37cd65a912da594709526ce822916b
Jessica Yang <jessicayang71@gmail.com>
2018-12-19 21:54:24 -0800

    new commit

--- app.py
+++ app.py
@@ -1,59 +1,77 @@
-from flask import Flask, request
-import json
-import requests
-
-app = Flask(__name__)
-
-# This needs to be filled with the Page Access Token that will be provided
-# by the Facebook App that will be created.
-PAT = 'EAADRzk5VTUYBADVxoxmYgj8zKyZAqPWLG3feVPubnDOSrRsOtV21jfGA4kJCy0NumgPBp8houEpj8Q45EgLrWNKYDxQvIROD1mnmgEN4Cz2C9i8SRleZAd9dEmHDmNDpzE5HcoFIYgbGMqDVmfSUA0e4w3SyngOdf98UlkjwZDZD'
+import praw
+import random
+import time
+import os
 
 
-@app.route('/', methods=['GET'])
-def handle_verification():
-    print("Handling Verification.")
-    if request.args.get('hub.verify_token') == 'my_voice_is_my_password_verify_me':
-        print("Verification successful!")
-        return request.args.get('hub.challenge')
-    else:
-        print("Verification failed!")
-        return 'Error, wrong validation token'
+def bot_loggin():
+    """"This function return your session in Reddit as an instance"""
+    reddit = praw.Reddit(username='******',
+                         password='*****',
+                         client_id='',
+                         client_secret='',
+                         user_agent='')
+    return reddit
 
 
-@app.route('/', methods=['POST'])
-def handle_messages():
-    print("Handling Messages")
-    payload = request.get_data()
-    print(payload)
-    for sender, message in messaging_events(payload):
-        print("Incoming from %s: %s" % (sender, message))
-        send_message(PAT, sender, message)
-    return "ok"
+def get_memes_bot(reddit):
+    """"Returns a list with the first most populars images in the subreddit /memes"""
+    jpg_url = []
+    for submission in reddit.subreddit('memes').top('all'):
+        if submission.url.endswith(".jpg"):
+            jpg_url.append(submission.url)
+            print(submission.url)
+    return jpg_url
 
 
-def messaging_events(payload):
+def get_thoughts_bot(reddit):
+    """"Returns a list with the first most populars images in the subreddit /Showerthoughts"""
+    jpg_url = []
+    for submission in reddit.subreddit('Showerthoughts').hot(limit=30):
+        jpg_url.append(submission.title)
+        print(submission.title)
 
-    data = json.loads(payload)
-    messaging_events = data["entry"][0]["messaging"]
-    for event in messaging_events:
-        if "message" in event and "text" in event["message"]:
-            yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-        else:
-            yield event["sender"]["id"], "I can't echo this"
+    return jpg_url
 
 
-def send_message(token, recipient, text):
-
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-                      params={"access_token": token},
-                      data=json.dumps({
-                          "recipient": {"id": recipient},
-                          "message": {"text": text.decode('unicode_escape')}
-                      }),
-                      headers={'Content-type': 'application/json'})
-    if r.status_code != requests.codes.ok:
-        print(r.text)
+def random_url(lst):
+    random_choice = random.choice(lst)
+    return random_choice
 
 
 if __name__ == '__main__':
-    app.run()
+
+    import webbrowser
+
+    reddit = bot_loggin()
+    if not reddit.read_only:
+        print("Logged to Reddit!!")
+    list_urls = get_memes_bot(reddit)
+    picture_day = random_url(list_urls)
+
+    while True:
+        number_pictures = len(list_urls)
+        if number_pictures == 1:
+            print("This is my last pick-me-up for you")
+            last_one = list_urls[-1]
+            webbrowser.open(last_one)
+            break
+
+        ask_user = raw_input("Are you having a bad day? Y or N ?")
+        if ask_user == "Y":     
+            print("I have %i memes to cheer you up" % (number_pictures))
+            check = raw_input("Do you want a meme? Y or N ?")
+            if check == "Y":
+                webbrowser.open(picture_day)
+                list_urls.remove(picture_day)
+                picture_day = random_url(list_urls)
+                print("I need a short break")
+                time.sleep(5)
+
+            else:
+                print("Ok, see you soon!")
+                break
+
+        else:
+            print("That's the spirit! Have a good one!")
+            break
